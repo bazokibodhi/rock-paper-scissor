e261aa28c72390aa77a0b8ee91c39a49490107a3
Bodhisatwya <bodhisatwya1492@gmail.com>
2025-08-21 02:06:55 +0530

    The function to get computer's choice is created

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // The entire javascript code goes down here
+
+// Creating function to get computer choice
+function getComputerChoice() {
+    let n = Math.floor(Math.random() * 100);
+
+    if (n <= 33) {
+        return 'Rock';
+    } else if (n <= 66) {
+        return 'Paper';
+    } else {
+        return 'Scissor';
+    }
+}
