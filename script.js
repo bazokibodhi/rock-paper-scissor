9b6dda60b700fbd1bba5153a192fc2b2f0610936
Bodhisatwya <bodhisatwya1492@gmail.com>
2025-08-21 02:17:17 +0530

    Created function to get human choice using promt

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // The entire javascript code goes down here
 
 // Creating function to get computer choice
+
 function getComputerChoice() {
     let n = Math.floor(Math.random() * 100);
 
@@ -10,5 +11,21 @@ function getComputerChoice() {
         return 'Paper';
     } else {
         return 'Scissor';
+    }
+}
+
+//  Creating function to get human choice
+
+function getHumanChoice() {
+    let select = Number(prompt("Press 1 to select Rock, 2 to select Paper & 3 to select Scissor"));
+
+    if (select === 1) {
+        return 'Rock';
+    } else if (select === 2) {
+        return 'Paper';
+    } else if (select === 3) {
+        return 'Scissor';
+    } else {
+        return 'Syntax Error';
     }
 }
